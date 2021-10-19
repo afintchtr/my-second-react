8b383abdcc73eefeed6800a87e4247bb51d91113
Afin Tachtiar <afintachtiar@apps.ipb.ac.id>
2021-10-19 17:15:23 +0700

    Commit 01
    This commit is a progress from Udemy React Course until SECTION 4 - REACT STATE and EVENT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e2",
+      title: "Toilet Paper",
+      amount: 5.99,
+      date: new Date(2021, 2, 26),
+    },
+    {
+      id: "e3",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 3, 10),
+    },
+    {
+      id: "e4",
+      title: "Console Game",
+      amount: 549.99,
+      date: new Date(2021, 3, 11),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
