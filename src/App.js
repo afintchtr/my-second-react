e0f59ef4726d9cb96fa01df7eb1cb1b083f85ff1
Afin Tachtiar <afintachtiar@apps.ipb.ac.id>
2021-10-21 06:49:36 +0700

    Commit 02
    Completing Course until Section 5, and that is the last section of Project 01 (Expenses List Tracker).

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "Toilet Paper",
+    amount: 5.99,
+    date: new Date(2021, 2, 26),
+  },
+  {
+    id: "e3",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 3, 10),
+  },
+  {
+    id: "e4",
+    title: "Console Game",
+    amount: 549.99,
+    date: new Date(2021, 3, 11),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "Toilet Paper",
-      amount: 5.99,
-      date: new Date(2021, 2, 26),
-    },
-    {
-      id: "e3",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 3, 10),
-    },
-    {
-      id: "e4",
-      title: "Console Game",
-      amount: 549.99,
-      date: new Date(2021, 3, 11),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense];
+    });
   };
 
   return (
